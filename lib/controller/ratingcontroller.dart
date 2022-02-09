2f6a9eecb00fa1b626583280236edf820a366f75
viviint <96400522+viviint@users.noreply.github.com>
2022-02-09 21:24:47 +0800

    ratingcontroller

--- lib/controller/ratingcontroller.dart
+++ lib/controller/ratingcontroller.dart
@@ -1,1 +1,21 @@
+import 'package:get/get.dart';
+import 'package:latihan_4/model/ratingmodel.dart';
+import 'package:latihan_4/service/ratingservice.dart';
 
+class RatingController extends GetxController {
+  RatingService _ratingService = RatingService();
+  var rate = <RatingModel>[];
+
+  @override
+  void onInit() {
+    getRate();
+    super.onInit();
+  }
+
+  void getRate() async {
+    _ratingService.getRatingModel().then((value) {
+      rate = value;
+      update();
+    });
+  }
+}
